2b30ae5c20e731a4ad6f00c8d07472c8ae8e7795
KlaraMartinsson <>
2023-08-04 20:51:48 +0000

    Make a new html for rule box

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-// Adding all necessary elements
-const startBtn = document.querySelector('.start-btn button');
-const rulesBox = document.querySelector('#rules-box');
-
-// If Start Quiz button is clicked
-startBtn.onclick = ()=>{
-    rulesBox.classList.add('active')
-};
