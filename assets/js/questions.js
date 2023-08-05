ecbd52b20a2d4a42ec26b13225a1b6d7e992cc7f
KlaraMartinsson <>
2023-08-05 14:42:23 +0000

    Add next button to quiz box

---
+++ assets/js/questions.js
@@ -0,0 +1,9 @@
+// Making arrays with all the questions
+const questions = [{
+    question: "In Australian slang, what do they mean by: she'll be right",
+    options: [
+        {text: "She is correct", correct: false}
+
+    ]
+}
+];
