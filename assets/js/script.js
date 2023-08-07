f6001169e42ca3dfa2700af700c247a4c766a484
KlaraMartinsson <>
2023-08-07 13:39:14 +0000

    Attempt to get the answers appear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,3 @@
-// Making arrays with all the questions
-const questions = [
-    {
-        question: "In Australian slang, what do they mean by: she'll be right?",
-        options: [
-            { text: "She is correct", correct: false },
-            { text: "She is feeling fine", correct: true },
-            { text: "Everything is okay", correct: false },
-            { text: "The weather is going to be okay", correct: false },
-        ]
-    },
-    {
-        question: "In Australian slang, what do they mean by: A cold one?",
-        options: [
-            { text: "A person who is not nice", correct: false },
-            { text: "The weather is cold", correct: false },
-            { text: "Beer", correct: true },
-            { text: "Soda", correct: false },
-        ]
-    },
-    {
-        question: "In Australian slang, what do they mean by: mate's rates?",
-        options: [
-            { text: "A discount from a friend", correct: true },
-            { text: "A rating scale of who you like the most of your friends", correct: false },
-            { text: "How much time you spend with a perticular friend", correct: false },
-            { text: "How much money you spend when you are with a perticular friend", correct: false },
-        ]
-    },
-    {
-        question: "In Australian slang, how do you say Mcdonald's?",
-        options: [
-            { text: "Mickey D's", correct: false },
-            { text: "Old Mac's", correct: false },
-            { text: "Macca's", correct: true },
-            { text: "Donald's", correct: false },
-        ]
-    },
-    {
-        question: "In Australian slang, what do they mean by: have a crack?",
-        options: [
-            { text: "Try to attempt something", correct: true },
-            { text: "Get a paper's cut", correct: false },
-            { text: "Visit the chiropractor", correct: false },
-            { text: "Start a fight", correct: false },
-        ]
-    },
-];
-
 //Getting all necessary elements
 
 const continueBtn = document.getElementById('continue-btn')
@@ -55,10 +6,7 @@ continueBtn.addEventListener('click', startQuiz)
 const quizBox = document.getElementsByClassName('quiz-box')[0]
 
 const questionTitle = document.getElementById('question-title')
-const options = document.getElementById('answer-buttons')
-
-//Gives random questions in the quiz
-let randomQuestions, currentQuestionIndex
+const optionList = document.getElementById('answer-buttons')
 
 // Hiding the rule box and showing question box, while clicking on continue button
 function startQuiz() {
@@ -70,10 +18,75 @@ function startQuiz() {
     giveNextQuestion()
 };
 
+//Gives random questions in the quiz
+let randomQuestions, currentQuestionIndex;
 function giveNextQuestion() {
     showQuestion(randomQuestions[currentQuestionIndex])
-};
+}
 
 function showQuestion(question) {
     questionTitle.innerText = question.question
-};
+    questions.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('answer-btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        optionList.appendChild(button)
+    })
+}
+
+function selectAnswer(event) {
+
+}
+
+// Making arrays with all the questions
+const questions = [
+    {
+        question: "In Australian slang, what do they mean by: she'll be right?",
+        answers: [
+            { text: "She is correct", correct: false },
+            { text: "She is feeling fine", correct: false },
+            { text: "Everything is okay", correct: true },
+            { text: "The weather is going to be okay", correct: false }
+        ]
+    },
+    {
+        question: "In Australian slang, what do they mean by: A cold one?",
+        answers: [
+            { text: "A person who is not nice", correct: false },
+            { text: "The weather is cold", correct: false },
+            { text: "Beer", correct: true },
+            { text: "Soda", correct: false }
+        ]
+    },
+    {
+        question: "In Australian slang, what do they mean by: mate's rates?",
+        answers: [
+            { text: "A discount from a friend", correct: true },
+            { text: "A rating scale of who you like the most of your friends", correct: false },
+            { text: "How much time you spend with a perticular friend", correct: false },
+            { text: "How much money you spend when you are with a perticular friend", correct: false }
+        ]
+    },
+    {
+        question: "In Australian slang, how do you say Mcdonald's?",
+        answers: [
+            { text: "Mickey D's", correct: false },
+            { text: "Old Mac's", correct: false },
+            { text: "Macca's", correct: true },
+            { text: "Donald's", correct: false }
+        ]
+    },
+    {
+        question: "In Australian slang, what do they mean by: have a crack?",
+        answers: [
+            { text: "Try to attempt something", correct: true },
+            { text: "Get a paper's cut", correct: false },
+            { text: "Visit the chiropractor", correct: false },
+            { text: "Start a fight", correct: false }
+        ]
+    }
+];
