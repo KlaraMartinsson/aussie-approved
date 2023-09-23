a13c7e0d0bbdcae57bb1d0335f3d02912de848e8
KlaraMartinsson <>
2023-09-23 13:10:58 +0000

    Erase the javascript link in index.html because it's not needed

---
+++ assets/js/questions.js
@@ -0,0 +1,139 @@
+// Making arrays with all the questions
+let questions = [
+    {
+        question: "In Australian slang, what do they mean by: she'll be right?",
+        answers: [
+            { text: "She is correct", correct: false },
+            { text: "She is feeling fine", correct: false },
+            { text: "Everything is okay", correct: true },
+            { text: "The weather is going to be okay", correct: false }
+        ]
+    },
+    {
+        question: "In Australian slang, what do they mean by: A cold one?",
+        answers: [
+            { text: "A person who is not nice", correct: false },
+            { text: "The weather is cold", correct: false },
+            { text: "Beer", correct: true },
+            { text: "Soda", correct: false }
+        ]
+    },
+    {
+        question: "In Australian slang, what do they mean by: mate's rates?",
+        answers: [
+            { text: "A discount from a friend", correct: true },
+            { text: "A rating scale of who you like the most of your friends", correct: false },
+            { text: "How much time you spend with a perticular friend", correct: false },
+            { text: "How much money you spend when you are with a perticular friend", correct: false }
+        ]
+    },
+    {
+        question: "In Australian slang, how do you say Mcdonald's?",
+        answers: [
+            { text: "Mickey D's", correct: false },
+            { text: "Old Mac's", correct: false },
+            { text: "Macca's", correct: true },
+            { text: "Donald's", correct: false }
+        ]
+    },
+    {
+        question: "In Australian slang, what do they mean by: have a crack?",
+        answers: [
+            { text: "Try to attempt something", correct: true },
+            { text: "Get a paper's cut", correct: false },
+            { text: "Visit the chiropractor", correct: false },
+            { text: "Start a fight", correct: false }
+        ]
+    },
+    {
+        question: "In Australian slang, what do they mean by: hard yakka?",
+        answers: [
+            { text: "Hard work", correct: true },
+            { text: "Hard helmet", correct: false },
+            { text: "Hard year", correct: false },
+            { text: "Hard life", correct: false }
+        ]
+    },
+    {
+        question: "In Australian slang, what do they mean by being: knackered?",
+        answers: [
+            { text: "Hungry", correct: false },
+            { text: "Excited", correct: false },
+            { text: "Tired", correct: true },
+            { text: "Angry", correct: false }
+        ]
+    },
+    {
+        question: "In Australian slang, what do they mean by: drinking goon?",
+        answers: [
+            { text: "Drinking cheap, boxed wine", correct: true },
+            { text: "Drinking homemade alcohol", correct: false },
+            { text: "Drinking is good", correct: false },
+            { text: "Drinking is done for today", correct: false }
+        ]
+    },
+
+    {
+        question: "In Australian slang, what do they mean by: thongs?",
+        answers: [
+            { text: "Tv", correct: false },
+            { text: "G-string", correct: false },
+            { text: "Computer", correct: false },
+            { text: "Flip flops", correct: true }
+        ]
+    },
+    {
+        question: "In Australian slang, what do they mean by: a dogs breakfast?",
+        answers: [
+            { text: "Ugly", correct: false },
+            { text: "A mess", correct: true },
+            { text: "Gross food", correct: false },
+            { text: "Quirky", correct: false }
+        ]
+    },
+    {
+        question: "In Australian slang, what do they mean by: barbie?",
+        answers: [
+            { text: "Barbie doll", correct: false },
+            { text: "Beach", correct: false },
+            { text: "Barbecue", correct: true },
+            { text: "Tiny person", correct: false }
+        ]
+    },
+    {
+        question: "In Australian slang, what do they mean by: dunny?",
+        answers: [
+            { text: "A diaper", correct: false },
+            { text: "A pantry", correct: false },
+            { text: "A toilet", correct: true },
+            { text: "A dustbin", correct: false }
+        ]
+    },
+    {
+        question: "In Australian slang, what do they mean by: Woop Woop?",
+        answers: [
+            { text: "The outback", correct: false },
+            { text: "In New South Wales", correct: false },
+            { text: "Somewhere far away", correct: true },
+            { text: "New Zealand", correct: false }
+        ]
+    },
+    {
+        question: "In Australian slang, what do they mean by: put a sock on it?",
+        answers: [
+            { text: "Go away", correct: false },
+            { text: "Shut up", correct: true },
+            { text: "Try harder", correct: false },
+            { text: "To fix something", correct: false }
+        ]
+    },
+    {
+        question: "In Australian slang, what do they mean by: mate?",
+        answers: [
+            { text: "Mature", correct: false },
+            { text: "Mankind", correct: false },
+            { text: "Friend", correct: true },
+            { text: "Feeling", correct: false }
+        ]
+    }
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,15 +9,13 @@ const feedbackBox = document.getElementsByClassName('feedback-box')[0];
 const countdownTimer = document.getElementById("countdown-timer");
 const questionTitle = document.getElementById('question-title');
 const answerList = document.getElementById('answer-buttons');
-
 const counterQuestions = document.getElementsByClassName('counter')[0];
+let scoreText = document.getElementsByClassName('score-text')[0];
 
-//Counts the questions in the quiz
 let questionCounters = 0;
 let questionNum = 1;
 let scores = 0;
-let scoreText = document.getElementsByClassName('score-text')[0];
-let time = 9999;
+let time = 0;
 let timer = setInterval(showTimer, 1000);
 
 
@@ -148,143 +146,3 @@ function showFeedback() {
     feedbackBox.classList.remove('hide'); // Shows the Feedback box
     scoreText.innerText = `Mate, you finished the quiz! You have scored ${scores}/15 points. Good luck on your new adventures in Australia!`;
 }
-
-// Making arrays with all the questions
-const questions = [
-    {
-        question: "In Australian slang, what do they mean by: she'll be right?",
-        answers: [
-            { text: "She is correct", correct: false },
-            { text: "She is feeling fine", correct: false },
-            { text: "Everything is okay", correct: true },
-            { text: "The weather is going to be okay", correct: false }
-        ]
-    },
-    {
-        question: "In Australian slang, what do they mean by: A cold one?",
-        answers: [
-            { text: "A person who is not nice", correct: false },
-            { text: "The weather is cold", correct: false },
-            { text: "Beer", correct: true },
-            { text: "Soda", correct: false }
-        ]
-    },
-    {
-        question: "In Australian slang, what do they mean by: mate's rates?",
-        answers: [
-            { text: "A discount from a friend", correct: true },
-            { text: "A rating scale of who you like the most of your friends", correct: false },
-            { text: "How much time you spend with a perticular friend", correct: false },
-            { text: "How much money you spend when you are with a perticular friend", correct: false }
-        ]
-    },
-    {
-        question: "In Australian slang, how do you say Mcdonald's?",
-        answers: [
-            { text: "Mickey D's", correct: false },
-            { text: "Old Mac's", correct: false },
-            { text: "Macca's", correct: true },
-            { text: "Donald's", correct: false }
-        ]
-    },
-    {
-        question: "In Australian slang, what do they mean by: have a crack?",
-        answers: [
-            { text: "Try to attempt something", correct: true },
-            { text: "Get a paper's cut", correct: false },
-            { text: "Visit the chiropractor", correct: false },
-            { text: "Start a fight", correct: false }
-        ]
-    },
-    {
-        question: "In Australian slang, what do they mean by: hard yakka?",
-        answers: [
-            { text: "Hard work", correct: true },
-            { text: "Hard helmet", correct: false },
-            { text: "Hard year", correct: false },
-            { text: "Hard life", correct: false }
-        ]
-    },
-    {
-        question: "In Australian slang, what do they mean by being: knackered?",
-        answers: [
-            { text: "Hungry", correct: false },
-            { text: "Excited", correct: false },
-            { text: "Tired", correct: true },
-            { text: "Angry", correct: false }
-        ]
-    },
-    {
-        question: "In Australian slang, what do they mean by: drinking goon?",
-        answers: [
-            { text: "Drinking cheap, boxed wine", correct: true },
-            { text: "Drinking homemade alcohol", correct: false },
-            { text: "Drinking is good", correct: false },
-            { text: "Drinking is done for today", correct: false }
-        ]
-    },
-
-    {
-        question: "In Australian slang, what do they mean by: thongs?",
-        answers: [
-            { text: "Tv", correct: false },
-            { text: "G-string", correct: false },
-            { text: "Computer", correct: false },
-            { text: "Flip flops", correct: true }
-        ]
-    },
-    {
-        question: "In Australian slang, what do they mean by: a dogs breakfast?",
-        answers: [
-            { text: "Ugly", correct: false },
-            { text: "A mess", correct: true },
-            { text: "Gross food", correct: false },
-            { text: "Quirky", correct: false }
-        ]
-    },
-    {
-        question: "In Australian slang, what do they mean by: barbie?",
-        answers: [
-            { text: "Barbie doll", correct: false },
-            { text: "Beach", correct: false },
-            { text: "Barbecue", correct: true },
-            { text: "Tiny person", correct: false }
-        ]
-    },
-    {
-        question: "In Australian slang, what do they mean by: dunny?",
-        answers: [
-            { text: "A diaper", correct: false },
-            { text: "A pantry", correct: false },
-            { text: "A toilet", correct: true },
-            { text: "A dustbin", correct: false }
-        ]
-    },
-    {
-        question: "In Australian slang, what do they mean by: Woop Woop?",
-        answers: [
-            { text: "The outback", correct: false },
-            { text: "In New South Wales", correct: false },
-            { text: "Somewhere far away", correct: true },
-            { text: "New Zealand", correct: false }
-        ]
-    },
-    {
-        question: "In Australian slang, what do they mean by: put a sock on it?",
-        answers: [
-            { text: "Go away", correct: false },
-            { text: "Shut up", correct: true },
-            { text: "Try harder", correct: false },
-            { text: "To fix something", correct: false }
-        ]
-    },
-    {
-        question: "In Australian slang, what do they mean by: mate?",
-        answers: [
-            { text: "Mature", correct: false },
-            { text: "Mankind", correct: false },
-            { text: "Friend", correct: true },
-            { text: "Feeling", correct: false }
-        ]
-    }
-];
